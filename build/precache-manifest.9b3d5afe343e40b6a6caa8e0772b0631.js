6803d095094f54bd4015922845e2de6204442fa4
cyanideayonara <cyanidesayonara@gmail.com>
2018-12-20 00:32:23 +0200

    remove folder 'build' from .gitignore

---
+++ build/precache-manifest.9b3d5afe343e40b6a6caa8e0772b0631.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dde5ea9c8f702f38e4f7",
+    "url": "/static/css/main.c0151b4c.chunk.css"
+  },
+  {
+    "revision": "dde5ea9c8f702f38e4f7",
+    "url": "/static/js/main.dde5ea9c.chunk.js"
+  },
+  {
+    "revision": "4e6bf972f77590ad30b6",
+    "url": "/static/js/1.4e6bf972.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "04a8ca79e7c6f8340fc44770dc7ab97c",
+    "url": "/index.html"
+  }
+];
